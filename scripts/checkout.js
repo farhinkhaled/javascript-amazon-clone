bcdf5f50692c23ca12479035311877b9325a33dc
Farhin Khaled <khaled.farhin@gmail.com>
2025-07-02 23:46:01 +0600

    create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { displayOrderSummary } from "./checkout/order-summary.js";
 import { displayPaymentSummary } from "./checkout/payment-summary.js";
+import "../data/cart-oop.js";
 
 displayOrderSummary();
 displayPaymentSummary();
