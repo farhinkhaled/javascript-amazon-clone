dacc7291ea5a3e566ced5484c638f62115b274cc
Farhin Khaled <khaled.farhin@gmail.com>
2025-07-07 20:39:57 +0600

    learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,45 @@
 import { displayOrderSummary } from "./checkout/order-summary.js";
 import { displayPaymentSummary } from "./checkout/payment-summary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import "../data/cart-class.js";
 // import '../data/backend-practice.js'
 
-loadProducts(() => {
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve();
+        });
+    }),
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        })
+    })
+]).then(() => {
     displayOrderSummary();
     displayPaymentSummary();
-});
+})
+
+// new Promise((resolve) => {
+//     loadProducts(() => {
+//         resolve();
+//     });
+// }).then(() => {
+//     return new Promise((resolve) => {
+//         loadCart(() => {
+//             resolve();
+//         })
+//     })
+// }).then(() => {
+//     displayOrderSummary();
+//     displayPaymentSummary();
+// });
+    
+// callback
+// loadProducts(() => {
+//     loadCart(() => {
+//         displayOrderSummary();
+//         displayPaymentSummary();
+//     })
+// });
