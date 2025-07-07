49e34277d1a45823a362a6905b008404679d7452
Farhin Khaled <khaled.farhin@gmail.com>
2025-07-07 16:35:26 +0600

    add backend-practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { displayOrderSummary } from "./checkout/order-summary.js";
 import { displayPaymentSummary } from "./checkout/payment-summary.js";
 // import "../data/cart-class.js";
+import '../data/backend-practice.js'
 
 displayOrderSummary();
 displayPaymentSummary();
